2783828f079dfb43fcf4af91ec627eafcf1755aa
Aleksei Malyshchik <aleksei.malyschik@gmail.com>
2024-04-30 19:24:09 +0500

    Исправлены компоненты и презентер

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
-import Presenter  from './presenter/presenter.js';
+import Presenter from './presenter/presenter.js';
+import { render, RenderPosition } from './render.js';
+import TripInfoView from './view/trip-info-view.js';
+import TripControlsView from './view/trip-controls-view.js';
 
 const siteMainElement = document.querySelector('.trip-events');
+const siteTripInfo = document.querySelector('.trip-main');
+const siteTripControls = document.querySelector('.trip-controls');
+
 const presenter = new Presenter({ container: siteMainElement });
+
+render(new TripInfoView, siteTripInfo, RenderPosition.AFTERBEGIN);
+render(new TripControlsView, siteTripControls, RenderPosition.AFTERBEGIN);
 
 presenter.init();
--- src/presenter/presenter.js
+++ src/presenter/presenter.js
@@ -1,20 +1,16 @@
-import { render } from "../render";
-import EventItem from "../view/event-item"
-import SortView from "../view/sort-view"
-import EventList from "../view/event-list"
-import FormAddView from "../view/form-add-view"
+import { render } from '../render';
+import EventListView from '../view/event-list-view';
+import SortView from '../view/sort-view';
+import FormAddView from '../view/form-add-view';
 
 export default class Presenter {
   constructor({container}){
     this.container = container;
   }
+
   init() {
     render(new SortView(), this.container);
-    render(new EventList(), this.container);
     render(new FormAddView(), this.container);
-    for (let i = 0; i < 3; i++) {
-      render(new EventItem(), this.container);
-  }
-    render(new EventList(), this.container);
+    render(new EventListView(),this.container);
   }
 }
