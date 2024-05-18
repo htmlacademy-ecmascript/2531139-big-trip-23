fd9c278ad91974de1f3d32448a9de0a3fb28e542
Aleksei Malyshchik <aleksei.malyschik@gmail.com>
2024-05-18 11:35:59 +0500

    Отредактирован презентер и main

--- src/presenter/presenter.js
+++ src/presenter/presenter.js
@@ -2,15 +2,43 @@ import { render } from '../render';
 import EventListView from '../view/event-list-view';
 import SortView from '../view/sort-view';
 import FormAddView from '../view/form-add-view';
+import FormEditView from '../view/form-edit-view';
+import EventItemView from '../view/event-item-view';
+import { EVENT_COUNT } from '../model/trip-model';
 
 export default class Presenter {
-  constructor({container}){
-    this.container = container;
+  constructor({container, tripModel}){
+    this.mainContainer = container;
+    this.eventListComponent = new EventListView();
+    this.points = [...tripModel.getPoints()];
+    this.destinations = [...tripModel.getDestinations()];
+    this.offers = [...tripModel.getOffers()];
   }
 
   init() {
-    render(new SortView(), this.container);
-    render(new FormAddView(), this.container);
-    render(new EventListView(),this.container);
+    console.log(this.offers);
+    console.log(this.destinations);
+    console.log(this.points);
+
+    render(new SortView(), this.mainContainer);
+    // render(new FormAddView(), this.mainContainer);
+    render(new FormEditView({
+      points: this.points[3],
+      destinations: this.destinations,
+      offers: this.offers,
+    }), this.mainContainer);
+    render(this.eventListComponent, this.mainContainer)
+
+    for(let i = 0; i < EVENT_COUNT; i++) {
+      render(new EventItemView(
+        {
+          points: this.points[i],
+          destinations: this.destinations,
+          offers: this.offers,
+        }
+      ),
+      this.eventListComponent.getElement()
+      );
   }
+}
 }
